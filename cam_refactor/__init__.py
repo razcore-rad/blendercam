e52ba06e979e46caeac415bd710e61ad8a1b39c7
Răzvan C. Rădulescu <razcore.rad@gmail.com>
2022-12-01 04:41:38 +0200

    Strategies and work area
    1st pass over strategies and added work area sub-panel to the
    operations panel.

--- cam_refactor/__init__.py
+++ cam_refactor/__init__.py
@@ -38,6 +38,10 @@ bl_info = {
 PRECISION = 5
 
 
+def get_icon(props: bpy.types.Property, propname: str) -> str:
+    return getattr(props, "ICON_MAP", {}).get(propname, "NONE")
+
+
 def get_enum_item_icon(items: list[tuple], item_type: str) -> str:
     return reduce(lambda acc, item: item[-2] if item_type == item[0] and len(item) == 5 else acc, items, "NONE")
 
@@ -68,16 +72,60 @@ def get_propnames(props: bpy.types.PropertyGroup, use_exclude_propnames=True):
     exclude_propnames = ["rna_type"]
     if use_exclude_propnames:
         exclude_propnames += getattr(props, "exclude_propnames", [])
-
     return sorted({propname for propname in props.rna_type.properties.keys() if propname not in exclude_propnames})
 
 
-def poll_mesh_object_source(self, obj: bpy.types.Object) -> bool:
-    return obj.type == "MESH"
+def poll_object_source(strategy: bpy.types.Property, obj: bpy.types.Object) -> bool:
+    curve = getattr(strategy, "curve", None)
+    return obj.users != 0 and obj.type in ["CURVE", "MESH"] and obj is not curve
+
+
+def poll_curve_object_source(strategy: bpy.types.Property, obj: bpy.types.Object) -> bool:
+    source = getattr(strategy, f"{strategy.source_type.lower()}_source", None)
+    return obj.users != 0 and obj.type == "CURVE" and obj is not source
+
+
+def poll_curve_limit(work_area: bpy.types.Property, obj: bpy.types.Object) -> bool:
+    result = False
+    scene = bpy.context.scene
+    try:
+        cam_job = scene.cam_jobs[scene.cam_job_active_index]
+        operation = cam_job.operations[cam_job.operation_active_index]
+        strategy = getattr(operation, f"{operation.strategy_type.lower()}_strategy")
+        curve = getattr(strategy, "curve", None)
+        result = poll_curve_object_source(strategy, obj) and obj is not curve
+    except IndexError:
+        pass
+    return result
 
 
 class CAM_Job(bpy.types.PropertyGroup):
     class Operation(bpy.types.PropertyGroup):
+        class WorkArea(bpy.types.PropertyGroup):
+            exclude_propnames = {"name"}
+
+            depth_start: bpy.props.FloatProperty(
+                name="Depth Start", default=0, min=0, max=1, precision=PRECISION, unit="LENGTH"
+            )
+            depth_end_type: bpy.props.EnumProperty(
+                name="Depth End Type",
+                items=[
+                    ("CUSTOM", "Custom", ""),
+                    ("OBJECT", "Object", ""),
+                    ("STOCK", "Stock", ""),
+                ],
+            )
+            depth_end: bpy.props.FloatProperty(
+                name="Depth End", default=1e-1, min=1e-5, max=1, precision=PRECISION, unit="LENGTH"
+            )
+            layer_size: bpy.props.FloatProperty(
+                name="Layer Size", default=0, min=1e-4, max=1e-1, precision=PRECISION, unit="LENGTH"
+            )
+            ambient: bpy.props.EnumProperty(
+                name="Ambient", items=[("ALL", "All", ""), ("AROUND", "Around", ""), ("OFF", "Off", "")]
+            )
+            curve_limit: bpy.props.PointerProperty(name="Curve Limit", type=bpy.types.Object, poll=poll_curve_limit)
+
         class DistanceAlongPathsMixin(bpy.types.PropertyGroup):
             distance_along_paths: bpy.props.FloatProperty(
                 name="Distance Along Paths",
@@ -113,31 +161,114 @@ class CAM_Job(bpy.types.PropertyGroup):
             exclude_propnames = [
                 "name",
                 "source_type",
-                "mesh_object_source",
+                "object_source",
                 "collection_source",
             ]
 
             source_type_items = [
-                ("MESH_OBJECT", "Mesh Object", "Mesh object data source.", "OUTLINER_OB_MESH", 0),
+                ("OBJECT", "Object", "Object data source.", "OBJECT_DATA", 0),
                 ("COLLECTION", "Collection", "Collection data source", "OUTLINER_COLLECTION", 1),
             ]
             source_type: bpy.props.EnumProperty(items=source_type_items, name="Source Type")
-            mesh_object_source: bpy.props.PointerProperty(
-                type=bpy.types.Object, name="Source", poll=poll_mesh_object_source
-            )
+            object_source: bpy.props.PointerProperty(type=bpy.types.Object, name="Source", poll=poll_object_source)
             collection_source: bpy.props.PointerProperty(type=bpy.types.Collection, name="Source")
 
         class BlockStrategy(DistanceAlongPathsMixin, DistanceBetweenPathsMixin, SourceMixin):
             pass
 
         class CarveProjectStrategy(DistanceAlongPathsMixin, SourceMixin):
-            pass
+            ICON_MAP = {"curve": "OUTLINER_OB_CURVE"}
+
+            curve: bpy.props.PointerProperty(name="Curve", type=bpy.types.Object, poll=poll_curve_object_source)
+            depth: bpy.props.FloatProperty(name="Depth", default=1e-3, unit="LENGTH")
 
         class CirclesStrategy(DistanceAlongPathsMixin, DistanceBetweenPathsMixin, SourceMixin):
             pass
 
         class CrossStrategy(DistanceAlongPathsMixin, DistanceBetweenPathsMixin, PathsAngleMixin, SourceMixin):
             pass
+
+        class CurveToPathStrategy(SourceMixin):
+            exclude_propnames = [
+                "name",
+                "source_type",
+                "curve_object_source",
+                "object_source",
+                "collection_source",
+            ]
+
+            source_type_items = [
+                ("CURVE_OBJECT", "Object (Curve)", "Curve object data source", "OUTLINER_OB_CURVE", 0),
+                ("COLLECTION", "Collection", "Collection data source", "OUTLINER_COLLECTION", 1),
+            ]
+            source_type: bpy.props.EnumProperty(items=source_type_items, name="Source Type")
+            curve_object_source: bpy.props.PointerProperty(
+                type=bpy.types.Object, name="Source", poll=poll_curve_object_source
+            )
+
+        class DrillStrategy(SourceMixin):
+            method: bpy.props.EnumProperty(
+                name="Method",
+                items=[
+                    ("POINTS", "Points", "Drill at every point", "SNAP_VERTEX", 0),
+                    (
+                        "CENTER",
+                        "Center",
+                        "Position is at the center of disjoint mesh or curve islands",
+                        "SNAP_FACE_CENTER",
+                        1,
+                    ),
+                ],
+            )
+
+        class MedialAxisStrategy(SourceMixin):
+            threshold: bpy.props.FloatProperty(name="Threshold", default=1e-3, unit="LENGTH")
+            subdivision: bpy.props.FloatProperty(name="Subdivision", default=2e-4, unit="LENGTH")
+            do_clean_finish: bpy.props.BoolProperty(name="Clean Finish", default=True)
+            do_generate_mesh: bpy.props.BoolProperty(name="Generate Mesh", default=True)
+
+        class OutlineFillStrategy(DistanceAlongPathsMixin, DistanceBetweenPathsMixin, SourceMixin):
+            pass
+
+        class PocketStrategy(DistanceBetweenPathsMixin, SourceMixin):
+            pass
+
+        class ProfileStrategy(SourceMixin):
+            cut_type: bpy.props.EnumProperty(
+                name="Cut",
+                items=[
+                    ("ON_LINE", "On Line", "On line"),
+                    ("INSIDE", "Inside", "Inside"),
+                    ("OUTSIDE", "Outside", "Outside"),
+                ],
+            )
+            do_merge: bpy.props.BoolProperty(name="Merge Outlines", default=True)
+            outlines_count: bpy.props.IntProperty(name="Outlines Count", default=0)
+            offset: bpy.props.IntProperty(name="Offset", default=0)
+            style: bpy.props.EnumProperty(
+                name="Style",
+                items=[
+                    ("CONVENTIONAL", "Conventional", "Conventional rounded"),
+                    ("OVERSHOOT", "Overshoot", "Overshoot style"),
+                ],
+            )
+
+        class ParallelStrategy(DistanceAlongPathsMixin, DistanceBetweenPathsMixin, PathsAngleMixin, SourceMixin):
+            pass
+
+        class SpiralStrategy(DistanceAlongPathsMixin, DistanceBetweenPathsMixin, SourceMixin):
+            pass
+
+        class WaterlineRoughingStrategy(DistanceBetweenPathsMixin, SourceMixin):
+            distance_between_slices: bpy.props.FloatProperty(
+                name="Distance Between Slices",
+                default=1e-3,
+                min=1e-5,
+                max=32,
+                precision=PRECISION,
+                unit="LENGTH",
+            )
+            fill_between_slices: bpy.props.BoolProperty(name="Fill Between Slices", default=True)
 
         name: bpy.props.StringProperty(default="Operation")
         is_hidden: bpy.props.BoolProperty(default=False)
@@ -177,17 +308,26 @@ class CAM_Job(bpy.types.PropertyGroup):
                 "Roughing below ZERO. Z is always below ZERO",
             ),
         ]
-        strategy_type: bpy.props.EnumProperty(items=strategy_type_items, name="Strategy Type")
+        strategy_type: bpy.props.EnumProperty(items=strategy_type_items, name="Strategy")
 
         block_strategy: bpy.props.PointerProperty(type=BlockStrategy)
         carve_project_strategy: bpy.props.PointerProperty(type=CarveProjectStrategy)
         circles_strategy: bpy.props.PointerProperty(type=CirclesStrategy)
         cross_strategy: bpy.props.PointerProperty(type=CrossStrategy)
+        curve_to_path_strategy: bpy.props.PointerProperty(type=CurveToPathStrategy)
+        drill_strategy: bpy.props.PointerProperty(type=DrillStrategy)
+        medial_axis_strategy: bpy.props.PointerProperty(type=MedialAxisStrategy)
+        outline_fill_strategy: bpy.props.PointerProperty(type=OutlineFillStrategy)
+        pocket_strategy: bpy.props.PointerProperty(type=PocketStrategy)
+        profile_strategy: bpy.props.PointerProperty(type=ProfileStrategy)
+        parallel_strategy: bpy.props.PointerProperty(type=ParallelStrategy)
+        spiral_strategy: bpy.props.PointerProperty(type=SpiralStrategy)
+        waterline_roughing_strategy: bpy.props.PointerProperty(type=WaterlineRoughingStrategy)
+
+        work_area: bpy.props.PointerProperty(type=WorkArea)
 
     name: bpy.props.StringProperty(name="Name", default="Job")
     is_hidden: bpy.props.BoolProperty(default=False)
-    do_simplify: bpy.props.BoolProperty(name="Simplify G-code", default=True)
-    do_export: bpy.props.BoolProperty(name="Export on compute", default=False)
     count: bpy.props.IntVectorProperty(name="Count", default=(1, 1), min=1, subtype="XYZ", size=2)
     gap: bpy.props.FloatVectorProperty(name="Gap", default=(0, 0), min=0, subtype="XYZ_LENGTH", size=2)
     operations: bpy.props.CollectionProperty(type=Operation)
@@ -205,6 +345,7 @@ class CAM_OT_Action(bpy.types.Operator):
         ("REMOVE_JOB", "Remove CAM job", "Remove CAM job"),
         ("MOVE_JOB", "Move CAM job", "Move CAM job"),
         ("COMPUTE_JOB", "Compute CAM job", "Compute CAM job"),
+        ("EXPORT_JOB", "Export CAM job", "Export CAM job"),
         ("ADD_OPERATION", "Add CAM operation", "Add CAM operation"),
         ("DUPLICATE_OPERATION", "Duplicate CAM operation", "Duplicate CAM operation"),
         ("REMOVE_OPERATION", "Remove CAM operation", "Remove CAM operation"),
@@ -304,11 +445,13 @@ class CAM_UL_List(bpy.types.UIList):
             layout.label(text="", icon_value=icon)
 
 
-class CAM_PT_Panel(bpy.types.Panel):
+class CAM_PT_PanelBase(bpy.types.Panel):
     bl_category = "CAM"
     bl_region_type = "UI"
     bl_space_type = "VIEW_3D"
 
+
+class CAM_PT_Panel(CAM_PT_PanelBase):
     def draw_list_row(self, list_id: str, dataptr, propname: str, active_propname: str, suffix: str) -> None:
         list_is_sortable = len(getattr(dataptr, propname)) > 1
         rows = 5 if list_is_sortable else 3
@@ -358,8 +501,6 @@ class CAM_PT_PanelJobs(CAM_PT_Panel):
             layout = self.layout
             row = layout.row()
             col = row.column(align=True)
-            col.prop(cam_job, "do_simplify")
-            col.prop(cam_job, "do_export")
 
             if all(count_coord == 1 for count_coord in cam_job.count):
                 col.prop(cam_job, "count")
@@ -371,16 +512,17 @@ class CAM_PT_PanelJobs(CAM_PT_Panel):
                 col = split.column()
                 col.prop(cam_job, "gap")
 
-            row = layout.row()
-            props = row.operator(CAM_OT_Action.bl_idname, text="Compute")
+            row = layout.row(align=True)
+            props = row.operator(CAM_OT_Action.bl_idname, text="Compute", icon="PLAY")
             props.type = "COMPUTE_JOB"
+
+            props = row.operator(CAM_OT_Action.bl_idname, text="Export", icon="EXPORT")
+            props.type = "EXPORT_JOB"
         except IndexError:
             pass
 
 
 class CAM_PT_PanelOperations(CAM_PT_Panel):
-    ICON_MAP = {"MESH_OBJECT": "OUTLINER_OB_MESH"}
-
     bl_label = "Operations"
     bl_parent_id = "CAM_PT_PanelJobs"
 
@@ -398,11 +540,11 @@ class CAM_PT_PanelOperations(CAM_PT_Panel):
             layout = self.layout
             layout.use_property_decorate = False
 
-            col = layout.column(align=True)
+            col = layout.box().column(align=True)
             col.prop(operation, "strategy_type")
-            strategy_attr = f"{operation.strategy_type.lower()}_strategy"
-            if hasattr(operation, strategy_attr):
-                props = getattr(operation, strategy_attr)
+            strategy_propname = f"{operation.strategy_type.lower()}_strategy"
+            if hasattr(operation, strategy_propname):
+                props = getattr(operation, strategy_propname)
                 col.row().prop(props, "source_type", expand=True)
                 col.prop(
                     props,
@@ -410,22 +552,81 @@ class CAM_PT_PanelOperations(CAM_PT_Panel):
                     icon=get_enum_item_icon(props.source_type_items, props.source_type),
                 )
                 for propname in get_propnames(props):
-                    col.prop(props, propname)
+                    col.prop(props, propname, icon=get_icon(props, propname))
+        except IndexError:
+            pass
+
+
+class CAM_PT_PanelOperationWorkArea(CAM_PT_PanelBase):
+    bl_label = "Work Area"
+    bl_parent_id = "CAM_PT_PanelOperations"
+
+    @classmethod
+    def poll(cls, context: bpy.types.Context) -> bool:
+        result = False
+        try:
+            scene = context.scene
+            cam_job = scene.cam_jobs[scene.cam_job_active_index]
+            operation = cam_job.operations[cam_job.operation_active_index]
+            strategy = getattr(operation, f"{operation.strategy_type.lower()}_strategy")
+            source_is_valid = getattr(strategy, f"{strategy.source_type.lower()}_source") is not None
+            if operation.strategy_type == "CARVE_PROJECT":
+                source_is_valid = source_is_valid and strategy.curve is not None
+            result = len(cam_job.operations) > 0 and source_is_valid
+        except IndexError:
+            pass
+        return result
+
+    def draw(self, context: bpy.types.Context) -> None:
+        try:
+            scene = context.scene
+            cam_job = context.scene.cam_jobs[scene.cam_job_active_index]
+            operation_work_area = cam_job.operations[cam_job.operation_active_index].work_area
+
+            layout = self.layout
+            layout.use_property_decorate = False
+
+            col = layout.box().column(align=True)
+            row = col.row(align=True)
+            row.prop(operation_work_area, "depth_start")
+            if operation_work_area.depth_end_type == "CUSTOM":
+                row.prop(operation_work_area, "depth_end")
+
+            row = col.row()
+            row.use_property_split = True
+            row.prop(operation_work_area, "depth_end_type", expand=True)
+
+            col = layout.column(align=True)
+            col.use_property_split = True
+            col.prop(operation_work_area, "layer_size")
+            col.prop(operation_work_area, "ambient", expand=True)
+            col.prop(operation_work_area, "curve_limit", icon="OUTLINER_OB_CURVE")
         except IndexError:
             pass
 
 
 classes = [
+    CAM_Job.Operation.WorkArea,
     CAM_Job.Operation.BlockStrategy,
     CAM_Job.Operation.CarveProjectStrategy,
     CAM_Job.Operation.CirclesStrategy,
     CAM_Job.Operation.CrossStrategy,
+    CAM_Job.Operation.CurveToPathStrategy,
+    CAM_Job.Operation.DrillStrategy,
+    CAM_Job.Operation.MedialAxisStrategy,
+    CAM_Job.Operation.OutlineFillStrategy,
+    CAM_Job.Operation.PocketStrategy,
+    CAM_Job.Operation.ProfileStrategy,
+    CAM_Job.Operation.ParallelStrategy,
+    CAM_Job.Operation.SpiralStrategy,
+    CAM_Job.Operation.WaterlineRoughingStrategy,
     CAM_Job.Operation,
     CAM_Job,
     CAM_OT_Action,
     CAM_UL_List,
     CAM_PT_PanelJobs,
     CAM_PT_PanelOperations,
+    CAM_PT_PanelOperationWorkArea,
 ]
 
 
